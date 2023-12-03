831809c7d8007292669399736184f7db6f55f4b1
YaelPrat <prat.yael@gmail.com>
2023-12-03 10:27:34 +0200

    Add solution for LeetCode problem marge 2 arrays

--- leetcode.js
+++ leetcode.js
@@ -5,6 +5,7 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
+//
 var merge = function(nums1, m, nums2, n) {
     let len= m+n;
     for(let i= len; i>= 1 ; i--){
