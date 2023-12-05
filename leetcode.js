9287e16fd99a81459942503c99253ca6cf83d91f
YaelPrat <prat.yael@gmail.com>
2023-12-05 09:27:48 +0200

    Add solution for LeetCode problem remove duplicates-easy

--- leetcode.js
+++ leetcode.js
@@ -1,68 +1,66 @@
 /**
  * @param {number[]} nums
- * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {
-    let i = 0;
-    let j = nums.length -1;
+var removeDuplicates = function(nums) {
+    if(nums.length === 0) return 0;
+    if(nums.length === 1) return 1;
 
-    while (i <= j) {
-        if(nums[i] === val){
-            if(nums[j] === val){
-                j--;
-            }
-            else{
-                nums[i] = nums[j] 
-                i++; j--;
-            }
+    let uniqueCounter = 1;
+    let j = 1;
+    for (let i = 0; i < nums.length-1; i++) {
+        while(nums[i]===nums[j] ){
+            j++;
         }
-        else{
-            i++
-        }
+        if (j === nums.length) {
+           return uniqueCounter;
+        } 
+        nums[i + 1] = nums[j];
+        uniqueCounter++;     
     }
-    return i;
-};
-
-// Helper function to sort the first k elements of nums
-var sort = function(nums, start, end) {
-    // Your sorting implementation (e.g., nums.sort((a, b) => a - b))
-    nums.slice(start, end).sort((a, b) => a - b);
-
+    return uniqueCounter;
 };
 
 // Test Case 1
-var nums1 = [3, 2, 2, 3];
-var val1 = 3;
-var expectedNums1 = [2, 2]; // Sorted array with no values equal to val1
-
-var nums2 = [];
-var val2 = 5;
-var expectedNums2 = [];
-
-
-var nums3 = [1, 1, 1, 1, 1];
-var val3 = 1;
-var expectedNums3 = [];
-
-var nums4 = [5, 2, 8, 7, 2, 4, 2, 6, 8];
-var val4 = 9;
-var expectedNums4 = [5, 2, 8, 7, 2, 4, 2, 6, 8];
-
-var nums5 = [5, 5, 5, 5, 5];
-var val5 = 5;
-var expectedNums5 = [];
-
-var k1 = removeElement(nums5, val5);
-
-
-// Debug information
-console.log('Modified nums:', nums5);
-console.log('Returned k:', k1);
-
-// console.assert(k1 === expectedNums3.length);
-// sort(nums3, 0, k1);
+// var nums1 = [1, 2];
+// var expectedNums1 = [1, 2];
+// var k1 = removeDuplicates(nums1);
+// console.log("k:" +k1);
+// console.log("a: "+ nums1);
+// // console.assert(k1 === expectedNums1.length);
 // for (var i = 0; i < k1; i++) {
-//     console.assert(nums3[i] === expectedNums3[i]);
+//     console.assert(nums1[i] === expectedNums1[i]);
 // }
 
+// // Test Case 2
+var nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+var expectedNums2 = [0, 1, 2, 3, 4];
+var k2 = removeDuplicates(nums2);
+console.log("k:" +k2);
+console.log("a: "+ nums2);
+console.assert(k2 === expectedNums2.length);
+for (var j = 0; j < k2; j++) {
+    console.assert(nums2[j] === expectedNums2[j]);
+}
+
+// // Additional Test Cases
+// var nums3 = [];
+// var expectedNums3 = [];
+// var k3 = removeDuplicates(nums3);
+// console.assert(k3 === expectedNums3.length);
+
+// var nums4 = [1];
+// var expectedNums4 = [1, _];
+// var k4 = removeDuplicates(nums4);
+// console.assert(k4 === expectedNums4.length);
+// for (var k = 0; k < k4; k++) {
+//     console.assert(nums4[k] === expectedNums4[k]);
+// }
+
+// var nums5 = [-1, 0, 0, 0, 0, 3, 3];
+// var expectedNums5 = [-1, 0, 3, _, _, _, _];
+// var k5 = removeDuplicates(nums5);
+// console.assert(k5 === expectedNums5.length);
+// for (var l = 0; l < k5; l++) {
+//     console.assert(nums5[l] === expectedNums5[l]);
+// }
