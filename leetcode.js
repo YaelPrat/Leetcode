4b30f254debe85c55a517bf17357af7bd7cf1d89
YaelPrat <prat.yael@gmail.com>
2023-12-06 10:00:19 +0200

    Add solution for LeetCode problem find majority element- easy

--- leetcode.js
+++ leetcode.js
@@ -2,65 +2,37 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-    if(nums.length === 0) return 0;
-    if(nums.length === 1) return 1;
-
-    let uniqueCounter = 1;
-    let j = 1;
-    for (let i = 0; i < nums.length-1; i++) {
-        while(nums[i]===nums[j] ){
-            j++;
-        }
-        if (j === nums.length) {
-           return uniqueCounter;
-        } 
-        nums[i + 1] = nums[j];
-        uniqueCounter++;     
-    }
-    return uniqueCounter;
+var majorityElement = function(nums) {
+    let n = nums.length;
+    if(n === 1) return nums[0];
+    if(n%2 ===1 ) n-=1; // case it is od number
+    
+    let sortedNums = nums.sort((a,b)=>a-b);
+    // console.log("the array: "+ sortedNums)
+    return sortedNums[n/2];
 };
 
-// Test Case 1
-// var nums1 = [1, 2];
-// var expectedNums1 = [1, 2];
-// var k1 = removeDuplicates(nums1);
-// console.log("k:" +k1);
-// console.log("a: "+ nums1);
-// // console.assert(k1 === expectedNums1.length);
-// for (var i = 0; i < k1; i++) {
-//     console.assert(nums1[i] === expectedNums1[i]);
-// }
+// Normal cases
+const nums1 = [3, 2, 3]; // Majority element is 3
+const nums2 = [2, 2, 1, 1, 1, 2, 2]; // Majority element is 2
+const nums3 = [1, 2, 2, 3, 2, 4, 2]; // Majority element is 2
 
-// // Test Case 2
-var nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
-var expectedNums2 = [0, 1, 2, 3, 4];
-var k2 = removeDuplicates(nums2);
-console.log("k:" +k2);
-console.log("a: "+ nums2);
-console.assert(k2 === expectedNums2.length);
-for (var j = 0; j < k2; j++) {
-    console.assert(nums2[j] === expectedNums2[j]);
-}
+// Edge cases
+const nums4 = [1]; // Single element array, majority element is 1
+const nums5 = [1, 1, 1, 1, 1, 1, 2, 2, 2]; // Majority element is 1
+const nums6 = [3, 3, 4, 2, 4, 4, 2, 4, 4]; // Majority element is 4
 
-// // Additional Test Cases
-// var nums3 = [];
-// var expectedNums3 = [];
-// var k3 = removeDuplicates(nums3);
-// console.assert(k3 === expectedNums3.length);
+// Large array with majority element
+const nums7 = Array.from({ length: 10000 }, (_, index) => index % 3 === 0 ? 42 : index % 3 === 1 ? 7 : 15);
 
-// var nums4 = [1];
-// var expectedNums4 = [1, _];
-// var k4 = removeDuplicates(nums4);
-// console.assert(k4 === expectedNums4.length);
-// for (var k = 0; k < k4; k++) {
-//     console.assert(nums4[k] === expectedNums4[k]);
-// }
+console.log(majorityElement(nums1)); // Expected output: 3 V
+console.log(majorityElement(nums3)); // Expected output: 2 V
+console.log(majorityElement(nums5)); // Expected output: 1 V
+console.log(majorityElement(nums6)); // Expected output: 4 V
 
-// var nums5 = [-1, 0, 0, 0, 0, 3, 3];
-// var expectedNums5 = [-1, 0, 3, _, _, _, _];
-// var k5 = removeDuplicates(nums5);
-// console.assert(k5 === expectedNums5.length);
-// for (var l = 0; l < k5; l++) {
-//     console.assert(nums5[l] === expectedNums5[l]);
-// }
+
+console.log(majorityElement(nums2)); // Expected output: 2 X
+
+console.log(majorityElement(nums4)); // Expected output: 1 X
+
+// console.log(majorityElement(nums7)); // Expected output: 42 X
